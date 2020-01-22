6ed37d4eed63c79edfa1f471c77b4d3a63f31499
Chmut Vladimir <bandanere@gmail.com>
2020-01-22 20:21:23 +0200

    comment

--- controllers/comment.php
+++ controllers/comment.php
@@ -4,9 +4,6 @@
     $comment = $_POST["comment"];
     $name = htmlspecialchars($name);
     $comment = htmlspecialchars($comment);
-    /*$mysqli = new mysqli("localhost", "root", "1q2w3e", "shop");
-    $mysqli->query("INSERT INTO comments (name, product_id, comment) VALUES 
-('$name', '$product_id', '$comment')");*/
     $link = getDbConnection();
     $result = mysqli_query($link, "INSERT INTO comments (name, product_id, comment) VALUES 
 ('$name', '$product_id', '$comment')");
